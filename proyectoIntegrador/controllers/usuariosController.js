a0c8c587fab5390f2db846a7ce68941223191f54
PedroSampayoMorrow <psampayomorrow@udesa.edu.ar>
2022-09-14 15:09:04 -0300

    Formularios y mi Perfil

--- proyectoIntegrador/controllers/usuariosController.js
+++ proyectoIntegrador/controllers/usuariosController.js
@@ -26,14 +26,18 @@ let usuariosController = {
         return res.render('login')
     },
     miPerfil: function (req, res) {
-        let idBuscado = req.params.id
-        let resultadoUsuario = []
-        for (let i = 0; i < data.usuarios.length; i++) {
-            if (idBuscado == data.usuarios[i].id) {
-                resultadoUsuario.push(data.usuarios[i])
+        let indiceRandom = Math.round(Math.random()*4)
+
+        let resultadoUsuario = data.usuarios[indiceRandom]
+        let resultadoUsuarioId = data.usuarios[indiceRandom].id
+        let posteosUsuario = []
+
+        for (let i = 0; i < data.posteos.length; i++) {
+            if (resultadoUsuarioId == data.posteos[i].idAutor) {
+                posteosUsuario.push(data.posteos[i])
             }
         }
-        return res.render('miPerfil', { usuario: resultadoUsuario[0] })
+        return res.render('miPerfil', { usuario: resultadoUsuario, posteosUsuario: posteosUsuario })
     },
     editarMiPerfil: function (req, res) {
         return res.render('editarPerfil')
